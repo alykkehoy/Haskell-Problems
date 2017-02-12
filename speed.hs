680b2772f4cc15f73dd5c809e43e12b0ae80177c
Anders <anders.lykkehoy1@marist.edu>
2017-02-12 17:18:56 -0500

    completed problem Speed Limit

--- speed.hs
+++ speed.hs
@@ -5,30 +5,31 @@ fix :: [(Int, Int)] -> Int -> [(Int, Int)]
 fix ((a, b):[]) n = [(a, b - n)]
 fix ((a, b):xs) n = [(a, b - n)] ++ fix xs n
 
---[(speed, total time)]
---convert [(20, 2), (30, 6), (10, 7)] sould be 170
 convert :: [(Int, Int)] -> Int
 convert ((a, b):[]) = a * b
 convert ((a, b):(c, d):xs) = a * b + convert ( fix ((c, d) : xs) b )
 
-getEntries :: Int -> [[Int]]
+getEntries :: Int -> IO [[Int]]
 getEntries 0 = return []
 getEntries n = do
   s <- getLine
-  ss <- getEntry s
+  ss <- return ( getEntry s )
   s2 <- getEntries (n - 1)
   return (ss : s2)
 
 makeLog :: [[Int]] -> [(Int, Int)]
 makeLog [] = []
-makeLog (s:xs) = do
-  ent <- ( head s, head ( tail s ) )
-  ent2 <- makeLog xs
-  return (ent : ent2)
+makeLog (s:xs) = (ent : ent2)
+  where
+    ent = ( head s, head ( tail s ) )
+    ent2 = makeLog xs
 
 
 main = do
   n <- readLn
-  log <- return ( getEntries n )
-  print ( convert ( makeLog log ) )
-  --print(convert [(20,2),(30,6),(10,7)])
+  if n /= (-1)
+     then do
+       log <- getEntries n
+       putStrLn ( show ( convert ( makeLog log ) ) ++ " miles")
+       main
+       else return ()
