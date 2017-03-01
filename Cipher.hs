dbab886c43690d6a9bf4ea455d392c6b7df141dc
Anders <anders.lykkehoy1@marist.edu>
2017-03-01 10:51:28 -0500

    added second way to shift char

--- Cipher.hs
+++ Cipher.hs
@@ -10,11 +10,17 @@ shiftChar c shift
   where
     newChar = fromEnum c + shift
 
+shiftChar2 :: Char -> Int -> Char
+shiftChar2 c 0 = c
+shiftChar2 'Z' shift = shiftChar2 'A' (shift - 1)
+shiftChar2 'z' shift = shiftChar2 'a' (shift - 1)
+shiftChar2 c shift = shiftChar2 (succ c) (shift - 1)
+
 toCaesar :: String -> Int -> String
 toCaesar [] _ = []
 toCaesar (x:xs) shift = newChar : toCaesar xs shift
   where
-    newChar = shiftChar x shift
+    newChar = shiftChar2 x shift
 
 unCaesar :: String -> Int -> String
 unCaesar xs shift = toCaesar xs (26 - shift)
